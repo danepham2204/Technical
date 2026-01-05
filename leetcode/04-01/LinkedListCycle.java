194c948f6d35acae3c7a6dd0481dba6d52fc461a
minh <hadtnt71@gmail.com>
2026-01-05 08:48:48 +0700

    Update code

--- leetcode/04-01/LinkedListCycle.java
+++ leetcode/04-01/LinkedListCycle.java
@@ -4,11 +4,13 @@ public class LinkedListCycle {
   static class ListNode {
     int val;
     ListNode next;
+
     ListNode(int x) {
       val = x;
       next = null;
     }
   }
+
   public boolean hasCycle(ListNode head) {
     if (head == null || head.next == null) {
       return false;
@@ -27,5 +29,17 @@ public class LinkedListCycle {
     }
 
     return false;
+  }
+
+  public static void main(String[] args) {
+    // Create a cycle: 3 -> 2 -> 0 -> -4 -> back to 2
+    ListNode head = new ListNode(3);
+    head.next = new ListNode(2);
+    head.next.next = new ListNode(0);
+    head.next.next.next = new ListNode(-4);
+    head.next.next.next.next = head.next; // cycle
+
+    LinkedListCycle solution = new LinkedListCycle();
+    System.out.println("Has cycle: " + solution.hasCycle(head)); // true
   }
 }
